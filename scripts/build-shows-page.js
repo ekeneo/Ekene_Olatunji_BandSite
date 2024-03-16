4540240d311d7c1b3156e5c620f4efc838c2e553
Ekene Olatunji <31261513+ekeneo@users.noreply.github.com>
2024-03-16 05:45:08 -0400

    add bio page form and comment list

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,24 @@
+{/* <section class="shows">
+<h3 class="shows__header">Shows</h3>
+<div class="shows__detail">
+  <table class="shows__table">
+    <tr class="shows__table--row">
+      <th class="shows__table--head">DATE</th>
+      <td class="shows__table--data shows__table--date">
+        Mon Sept 09 2024
+      </td>
+    </tr>
+    <tr class="shows__table--row">
+      <th class="shows__table--head">VENUE</th>
+      <td class="shows__table--data">Ronald Lane</td>
+    </tr>
+    <tr class="shows__table--row">
+      <th class="shows__table--head">LOCATION</th>
+      <td class="shows__table--data">San Francisco, CA</td>
+    </tr>
+  </table>
+</div>
+<div class="button-container">
+  <button class="shows__btn" id="buy-ticket-btn">BUY TICKETS</button>
+</div>
+</section> */}
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,41 +1,111 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const addCommentButton = document.getElementById('add-comment-btn');
-  const commentsList = document.getElementById('comments-list');
+// list of default comment entries
+// array of (comment) objects: name, comment, date
+const commentEntries = [
+  { name: 'Victor Pinto', comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' , date: '03/13/2024'},
+  { name: 'Christina Cabrera', comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.' , date: '03/13/2024'},
+  { name: 'Isaac Tedesse', comment: 'This is the first comment.I cant stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Cant get enough.' , date: '03/13/2024'}
+];
+console.log('commentEntries: ', commentEntries);
 
-  // Default comments
-  const defaultComments = [
-    { name: 'Isaac Tedesse', comment: 'This is the first comment.I cant stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Cant get enough.' },
-    { name: 'Christina Cabrera', comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.' },
-    { name: 'Victor Pinto', comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' }
-  ];
+// ===html code for reference===
+/* <section class="comments">
+  <article class="comments-list" id="comments-list-id">
+    <div class="comments-list__icon"></div>
+    <div class="comments-list__group">
+      <div class="comments-list__group2">
+        <p class="comments-list__name">Victor Pinto</p>
+        <date class="comments-list__date">11/02/2023</date>
+      </div>
+      <p class="comments-list__comment">
+        This is art. This is inexplicable magic expressed in the purest way,
+        everything that makes up this majestic work deserves reverence. Let
+        us appreciate this for what it is and what it contains.
+      </p>
+    </div>
+  </article>
+</section>   */
 
-  // Function to display comments
-  function displayComments(comments) {
-    commentsList.innerHTML = '';
-    comments.forEach(comment => {
-      const commentElement = document.createElement('div');
-      commentElement.classList.add('comment');
-      commentElement.innerHTML = `
-        <span>${comment.name}</span>
-        <p>${comment.comment}</p>
-      `;
-      commentsList.prepend(commentElement); // Newest comments at the top
-    });
-  }
 
-  // Display default comments when page loads
-  displayComments(defaultComments);
+// selecting the <section> tag which is a container for the comment section
+const commentEntriesContainer = document.querySelector('.comments');
+console.log('commentEntriesContainer: ', commentEntriesContainer);
 
-  // Event listener for Add Comment button
-  addCommentButton.addEventListener('click', function() {
-    const name = prompt('Enter your name:');
-    const comment = prompt('Enter your comment:');
-    if (name && comment) {
-      const newComment = { name, comment };
-      defaultComments.unshift(newComment); // Add new comment at the beginning
-      displayComments(defaultComments);
-    } else {
-      alert('Please enter both your name and comment.');
-    }
+// making the for each a function to recall 
+function renderCommentEntries() {
+ // To avoid repeating array after every render/append, remove all child elements from an element
+  commentEntriesContainer.innerHTML = '';
+
+  // for each comment object, create a new comment element
+  commentEntries.forEach((commentItem) => {
+    console.log('commentItem: ', commentItem);
+
+    // article tag <article class="comments-list">
+    const commentArticleEl = document.createElement("div");
+    commentArticleEl.classList.add('comments-list');
+    commentEntriesContainer.appendChild(commentArticleEl);
+
+    // Icon <div class="comments-list__icon">
+    const commentIconEl = document.createElement("div");
+    commentIconEl.classList.add('comments-list__icon');
+    commentArticleEl.appendChild(commentIconEl);
+
+    // Group <div class="comments-list__group">
+    const commentGroupEl = document.createElement("div");
+    commentGroupEl.classList.add('comments-list__group');
+    commentArticleEl.appendChild(commentGroupEl);
+
+    // Group 2 <div class="comments-list__group2">
+    const commentGroup2El = document.createElement("div");
+    commentGroup2El.classList.add('comments-list__group2');
+    commentGroupEl.appendChild(commentGroup2El);
+    
+    // create name <p class="comments-list__name">Victor Pinto</p>
+    const commentNameEl = document.createElement('p');
+    commentNameEl.classList.add('comments-list__name');
+    commentNameEl.innerText = commentItem.name;
+    commentGroup2El.appendChild(commentNameEl);
+
+    // create date <date class="comments-list__date">11/02/2023</date>
+    const commentDateEl = document.createElement('date');
+    commentDateEl.classList.add('comments-list__date');
+    commentDateEl.innerText = commentItem.date;
+    commentGroup2El.appendChild(commentDateEl);
+
+    // create comment <p class="comments-list__comment">
+    const commentTextEl = document.createElement('p');
+    commentTextEl.classList.add('comments-list__comment');
+    commentTextEl.innerText = commentItem.comment;
+    commentGroupEl.appendChild(commentTextEl);
+
+    console.log("commentGroupEl: ", commentGroupEl);
   });
+}
+
+// Form event handling callback
+const formEl = document.querySelector('.comments-form');
+
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const nameVal = event.target.nameId.value;
+  const commentVal = event.target.commentId.value;
+
+  const newEntry = {
+    name: nameVal,
+    comment: commentVal
+    // date: dateVal
+  };
+
+  console.log('newEntry: ', newEntry);
+
+  commentEntries.push(newEntry);
+
+  renderCommentEntries();
+
+  // Clear all form fields from the form after submitting
+  event.target.reset();
+
 });
+
+// To invoke the function to render initial comment entries on page load
+renderCommentEntries();
