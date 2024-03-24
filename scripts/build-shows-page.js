43b92136dca12da087369cbc9c288435bce03479
Ekene Olatunji <31261513+ekeneo@users.noreply.github.com>
2024-03-23 23:40:15 -0400

    Add GET function to Bio page and Shows page to retrieve data from API

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,43 +1,12 @@
-// === html for reference ===
-/* <section class="shows-container">
-      <h3 class="shows_header">Shows</h3>
-      <div class="shows">
-        <div class="shows-head">
-          <p class="shows-head__date">DATE</p>
-          <p class="shows-head__venue">VENUE</p>
-          <p class="shows-head__location">LOCATION</p>
-        </div>
-        <div class="shows-data">
-          <p class="shows-data__date">Mon Sep 09 2024</p>
-          <p class="shows-data__venue">Ronald Lane</p>
-          <p class="shows-data__location">San Francisco, CA</p>
-          <button class="shows-data__btn">BUY TICKETS</button>
-        </div>
-      </div>
-    </section> */
-
-
-// list of default show entries
-// array of (shows) objects: date, venue, location
-const showEntries = [
-  { date: 'Mon Sep 09 2024', venue: 'Ronald Lane' , location: 'San Francisco, CA'},
-  { date: 'Tue Sept 17 2024', venue: 'Pier 3 East' , location: 'San Francisco, CA'},
-  { date: 'Sat Oct 12 2024', venue: 'View Lounge' , location: 'San Francisco, CA'},
-  { date: 'Sat Nov 16 2024', venue: 'Hyatt Agency' , location: 'San Francisco, CA'},
-  { date: 'Fri Nov 29 2024', venue: 'Moscow Center' , location: 'San Francisco, CA'},
-  { date: 'Wed Dec 18 2024', venue: 'Press Club' , location: 'San Francisco, CA'}
-];
-console.log('showEntries: ', showEntries);
-
-// selecting the <section> tag which is a container for the shows section
+// === Sprint 3 begin ===
 const showEntriesContainer = document.querySelector('.shows-container');
-console.log('showEntriesContainer: ', showEntriesContainer);
+const apiKey = "35c9c5f2-6e06-48a3-913c-08f4571c2416";
+const bandSiteApi = new BandSiteApi(apiKey);
 
-function rendershowEntries() {
-  // for each show object, create a new show element
-  showEntries.forEach((showItem) => {
-    
-    // create shows <div class="shows">
+console.log(bandSiteApi);
+
+function renderShowEntries(show) {
+  // create shows <div class="shows">
     const showsEl = document.createElement("div");
     showsEl.classList.add('shows');
     showEntriesContainer.appendChild(showsEl);
@@ -73,19 +42,19 @@ function rendershowEntries() {
     // create shows-data__date <p class="shows-data__date">Mon Sep 09 2024</p>
     const showsDateEl = document.createElement("p");
     showsDateEl.classList.add('shows-data__date');
-    showsDateEl.textContent = showItem.date; 
+    showsDateEl.textContent = show.date; 
     showsDataEl.appendChild(showsDateEl);
 
     // create shows-data__venue <p class="shows-data__venue">Ronald Lane</p>
     const showsVenueEl = document.createElement("p");
     showsVenueEl.classList.add('shows-data__venue');
-    showsVenueEl.textContent = showItem.venue; 
+    showsVenueEl.textContent = show.place; 
     showsDataEl.appendChild(showsVenueEl);
 
     // create shows-data__location <p class="shows-data__location">San Francisco, CA</p>
     const showsLocationEl = document.createElement("p");
     showsLocationEl.classList.add('shows-data__location');
-    showsLocationEl.textContent = showItem.location; 
+    showsLocationEl.textContent = show.location; 
     showsDataEl.appendChild(showsLocationEl);
 
     //  create Buy Tickets Button <button class="shows-data__btn">BUY TICKETS</button>
@@ -93,7 +62,131 @@ function rendershowEntries() {
     showsBtnEl.classList.add('shows-data__btn');
     showsBtnEl.textContent = "BUY TICKETS"; 
     showsDataEl.appendChild(showsBtnEl);
-  });
+    
+    console.log('showsEl: ', showsEl);
 }
 
-rendershowEntries() 
+const getShows = async () => {
+  try {
+    showEntriesContainer.innerText = '...loading shows ...';
+
+    const showData = await bandSiteApi.getShows();
+    // To avoid repeating array after every render/append, remove all child elements from an element
+    showEntriesContainer.innerHTML = '';
+        
+    // Now that we have data from our API, let's use it to populate content
+    showData.forEach((show) => {
+      // render the elements for each show object
+      renderShowEntries(show);
+    });
+  } catch(error) {
+    showEntriesContainer.innerText = "Sorry, we couldn't fetch your data at the moment";
+  }
+}
+getShows();
+
+//  ==== Sprint 3 end ===
+
+
+// === Sprint 2 Begin - Kept for future reference===
+// === html for reference ===
+/* <section class="shows-container">
+      <h3 class="shows_header">Shows</h3>
+      <div class="shows">
+        <div class="shows-head">
+          <p class="shows-head__date">DATE</p>
+          <p class="shows-head__venue">VENUE</p>
+          <p class="shows-head__location">LOCATION</p>
+        </div>
+        <div class="shows-data">
+          <p class="shows-data__date">Mon Sep 09 2024</p>
+          <p class="shows-data__venue">Ronald Lane</p>
+          <p class="shows-data__location">San Francisco, CA</p>
+          <button class="shows-data__btn">BUY TICKETS</button>
+        </div>
+      </div>
+    </section> */
+
+
+// // list of default show entries
+// // array of (shows) objects: date, venue, location
+// const showEntries = [
+//   { date: 'Mon Sep 09 2024', venue: 'Ronald Lane' , location: 'San Francisco, CA'},
+//   { date: 'Tue Sept 17 2024', venue: 'Pier 3 East' , location: 'San Francisco, CA'},
+//   { date: 'Sat Oct 12 2024', venue: 'View Lounge' , location: 'San Francisco, CA'},
+//   { date: 'Sat Nov 16 2024', venue: 'Hyatt Agency' , location: 'San Francisco, CA'},
+//   { date: 'Fri Nov 29 2024', venue: 'Moscow Center' , location: 'San Francisco, CA'},
+//   { date: 'Wed Dec 18 2024', venue: 'Press Club' , location: 'San Francisco, CA'}
+// ];
+// console.log('showEntries: ', showEntries);
+
+// // selecting the <section> tag which is a container for the shows section
+// const showEntriesContainer = document.querySelector('.shows-container');
+// console.log('showEntriesContainer: ', showEntriesContainer);
+
+// function rendershowEntries() {
+//   // for each show object, create a new show element
+//   showEntries.forEach((showItem) => {
+    
+//     // create shows <div class="shows">
+//     const showsEl = document.createElement("div");
+//     showsEl.classList.add('shows');
+//     showEntriesContainer.appendChild(showsEl);
+
+//     // create shows-head <div class="shows-head">
+//     const showsHeadEl = document.createElement("div");
+//     showsHeadEl.classList.add('shows-head');
+//     showsEl.appendChild(showsHeadEl);
+
+//     // create DATE <p class="shows-head__date">DATE</p>
+//     const showDateEl = document.createElement("p");
+//     showDateEl.classList.add('shows-head__date');
+//     showDateEl.textContent = "DATE"; 
+//     showsHeadEl.appendChild(showDateEl);
+
+//     // create VENUE <p class="shows-head__venue">VENUE</p>
+//     const showVenueEl = document.createElement("p");
+//     showVenueEl.classList.add('shows-head__venue');
+//     showVenueEl.textContent = "VENUE"; 
+//     showsHeadEl.appendChild(showVenueEl);
+
+//     // create LOCATION <p class="shows-head__location">LOCATION</p>
+//     const showLocationEl = document.createElement("p");
+//     showLocationEl.classList.add('shows-head__location');
+//     showLocationEl.textContent = "LOCATION"; 
+//     showsHeadEl.appendChild(showLocationEl);
+
+//     // create shows-data <div class="shows-data">
+//     const showsDataEl = document.createElement("div");
+//     showsDataEl.classList.add('shows-data');
+//     showsEl.appendChild(showsDataEl);
+
+//     // create shows-data__date <p class="shows-data__date">Mon Sep 09 2024</p>
+//     const showsDateEl = document.createElement("p");
+//     showsDateEl.classList.add('shows-data__date');
+//     showsDateEl.textContent = showItem.date; 
+//     showsDataEl.appendChild(showsDateEl);
+
+//     // create shows-data__venue <p class="shows-data__venue">Ronald Lane</p>
+//     const showsVenueEl = document.createElement("p");
+//     showsVenueEl.classList.add('shows-data__venue');
+//     showsVenueEl.textContent = showItem.venue; 
+//     showsDataEl.appendChild(showsVenueEl);
+
+//     // create shows-data__location <p class="shows-data__location">San Francisco, CA</p>
+//     const showsLocationEl = document.createElement("p");
+//     showsLocationEl.classList.add('shows-data__location');
+//     showsLocationEl.textContent = showItem.location; 
+//     showsDataEl.appendChild(showsLocationEl);
+
+//     //  create Buy Tickets Button <button class="shows-data__btn">BUY TICKETS</button>
+//     const showsBtnEl = document.createElement("button");
+//     showsBtnEl.classList.add('shows-data__btn');
+//     showsBtnEl.textContent = "BUY TICKETS"; 
+//     showsDataEl.appendChild(showsBtnEl);
+//   });
+// }
+
+// rendershowEntries() 
+
+// // ==== Sprint 2 end ===
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,125 +1,195 @@
 // ===Sprint 3 begin ===
-
-// console.log(bandSiteApi);
-
-// ==== Sprint 2 begin ===
-// list of default comment entries
-// array of (comment) objects: name, comment, date
-const commentEntries = [
-  { name: 'Victor Pinto', comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' , date: '03/13/2024'},
-  { name: 'Christina Cabrera', comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.' , date: '03/13/2024'},
-  { name: 'Isaac Tedesse', comment: 'This is the first comment.I cant stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Cant get enough.' , date: '03/13/2024'}
-];
-console.log('commentEntries: ', commentEntries);
-
-// ===html code for reference===
-/* <section class="comments">
-  <article class="comments-list" id="comments-list-id">
-    <div class="comments-list__icon"></div>
-    <div class="comments-list__group">
-      <div class="comments-list__group2">
-        <p class="comments-list__name">Victor Pinto</p>
-        <date class="comments-list__date">11/02/2023</date>
-      </div>
-      <p class="comments-list__comment">
-        This is art. This is inexplicable magic expressed in the purest way,
-        everything that makes up this majestic work deserves reverence. Let
-        us appreciate this for what it is and what it contains.
-      </p>
-    </div>
-  </article>
-</section>   */
-
-
-// selecting the <section> tag which is a container for the comment section
 const commentEntriesContainer = document.querySelector('.comments');
-console.log('commentEntriesContainer: ', commentEntriesContainer);
+const apiKey = "35c9c5f2-6e06-48a3-913c-08f4571c2416";
+const bandSiteApi = new BandSiteApi(apiKey);
 
-// making the for each a function to recall 
-function renderCommentEntries() {
- // To avoid repeating array after every render/append, remove all child elements from an element
-  commentEntriesContainer.innerHTML = '';
+console.log(bandSiteApi);
 
-  // for each comment object, create a new comment element
-  commentEntries.forEach((commentItem) => {
-    console.log('commentItem: ', commentItem);
+function renderCommentEntries(comment) {
+  // article tag <article class="comments-list">
+  const commentArticleEl = document.createElement("div");
+  commentArticleEl.classList.add('comments-list');
+  commentEntriesContainer.appendChild(commentArticleEl);
 
-    // article tag <article class="comments-list">
-    const commentArticleEl = document.createElement("div");
-    commentArticleEl.classList.add('comments-list');
-    commentEntriesContainer.appendChild(commentArticleEl);
+  // Icon <div class="comments-list__icon">
+  const commentIconEl = document.createElement("div");
+  commentIconEl.classList.add('comments-list__icon');
+  commentArticleEl.appendChild(commentIconEl);
 
-    // Icon <div class="comments-list__icon">
-    const commentIconEl = document.createElement("div");
-    commentIconEl.classList.add('comments-list__icon');
-    commentArticleEl.appendChild(commentIconEl);
+  // Group <div class="comments-list__group">
+  const commentGroupEl = document.createElement("div");
+  commentGroupEl.classList.add('comments-list__group');
+  commentArticleEl.appendChild(commentGroupEl);
 
-    // Group <div class="comments-list__group">
-    const commentGroupEl = document.createElement("div");
-    commentGroupEl.classList.add('comments-list__group');
-    commentArticleEl.appendChild(commentGroupEl);
-
-    // Group 2 <div class="comments-list__group2">
-    const commentGroup2El = document.createElement("div");
-    commentGroup2El.classList.add('comments-list__group2');
-    commentGroupEl.appendChild(commentGroup2El);
+  // Group 2 <div class="comments-list__group2">
+  const commentGroup2El = document.createElement("div");
+  commentGroup2El.classList.add('comments-list__group2');
+  commentGroupEl.appendChild(commentGroup2El);
     
-    // create name <p class="comments-list__name">Victor Pinto</p>
-    const commentNameEl = document.createElement('p');
-    commentNameEl.classList.add('comments-list__name');
-    commentNameEl.innerText = commentItem.name;
-    commentGroup2El.appendChild(commentNameEl);
+  // create name <p class="comments-list__name">Victor Pinto</p>
+  const commentNameEl = document.createElement('p');
+  commentNameEl.classList.add('comments-list__name');
+  commentNameEl.innerText = comment.name;
+  commentGroup2El.appendChild(commentNameEl);
 
-    // create date <date class="comments-list__date">11/02/2023</date>
-    const commentDateEl = document.createElement('date');
-    commentDateEl.classList.add('comments-list__date');
-    commentDateEl.innerText = commentItem.date;
-    commentGroup2El.appendChild(commentDateEl);
+  // create date <date class="comments-list__date">11/02/2023</date>
+  const commentDateEl = document.createElement('date');
+  commentDateEl.classList.add('comments-list__date');
+  commentDateEl.innerText = comment.timestamp; 
+  commentGroup2El.appendChild(commentDateEl);
 
-    // create comment <p class="comments-list__comment">
-    const commentTextEl = document.createElement('p');
-    commentTextEl.classList.add('comments-list__comment');
-    commentTextEl.innerText = commentItem.comment;
-    commentGroupEl.appendChild(commentTextEl);
+  // create comment <p class="comments-list__comment">
+  const commentTextEl = document.createElement('p');
+  commentTextEl.classList.add('comments-list__comment');
+  commentTextEl.innerText = comment.comment;
+  commentGroupEl.appendChild(commentTextEl);
 
-    console.log("commentGroupEl: ", commentGroupEl);
-  });
+  console.log("commentGroupEl: ", commentGroupEl);
 }
 
-// To generate dynamic date
-function getCurrentDate() {
-  var date = new Date();
-  var options = { year: 'numeric', month: 'numeric', day: 'numeric' };
-  return date.toLocaleDateString('en-US', options);
+
+const getComments = async () => {
+  try {
+    commentEntriesContainer.innerText = '...loading comments ...';
+
+    const commentData = await bandSiteApi.getComments();
+    // To avoid repeating array after every render/append, remove all child elements from an element
+    commentEntriesContainer.innerHTML = '';
+        
+    // Now that we have data from our API, let's use it to populate content
+    commentData.forEach((comment) => {
+      // render the elements for each comment object
+      renderCommentEntries(comment);
+    });
+  } catch(error) {
+    commentEntriesContainer.innerText = "Sorry, we couldn't fetch your data at the moment";
+  }
 }
+getComments();
 
-// Form event handling callback
-const formEl = document.querySelector('.comments-form');
 
-formEl.addEventListener('submit', (event) => {
-  event.preventDefault();
+// ===Sprint 3 end ===
 
-  const nameVal = event.target.nameId.value;
-  const commentVal = event.target.commentId.value;
-  const dateVal = getCurrentDate();
 
-  const newEntry = {
-    name: nameVal,
-    comment: commentVal,
-    date: dateVal
-  };
+// // ==== Sprint 2 begin ===
+// // list of default comment entries
+// // array of (comment) objects: name, comment, date
+// const commentEntries = [
+//   { name: 'Victor Pinto', comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' , date: '03/13/2024'},
+//   { name: 'Christina Cabrera', comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.' , date: '03/13/2024'},
+//   { name: 'Isaac Tedesse', comment: 'This is the first comment.I cant stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Cant get enough.' , date: '03/13/2024'}
+// ];
+// console.log('commentEntries: ', commentEntries);
 
-  console.log('newEntry: ', newEntry);
+// // ===html code for reference===
+// /* <section class="comments">
+//   <article class="comments-list" id="comments-list-id">
+//     <div class="comments-list__icon"></div>
+//     <div class="comments-list__group">
+//       <div class="comments-list__group2">
+//         <p class="comments-list__name">Victor Pinto</p>
+//         <date class="comments-list__date">11/02/2023</date>
+//       </div>
+//       <p class="comments-list__comment">
+//         This is art. This is inexplicable magic expressed in the purest way,
+//         everything that makes up this majestic work deserves reverence. Let
+//         us appreciate this for what it is and what it contains.
+//       </p>
+//     </div>
+//   </article>
+// </section>   */
 
-  commentEntries.push(newEntry);
 
-  renderCommentEntries();
+// // selecting the <section> tag which is a container for the comment section
+// const commentEntriesContainer = document.querySelector('.comments');
+// // console.log('commentEntriesContainer: ', commentEntriesContainer);
 
-  // Clear all form fields from the form after submitting
-  event.target.reset();
+// // making the for each a function to recall 
+// function renderCommentEntries() {
+//  // To avoid repeating array after every render/append, remove all child elements from an element
+//   commentEntriesContainer.innerHTML = '';
 
-});
+//   // for each comment object, create a new comment element
+//   commentEntries.forEach((commentItem) => {
+//     console.log('commentItem: ', commentItem);
 
-// To invoke the function to render initial comment entries on page load
-renderCommentEntries();
+//     // article tag <article class="comments-list">
+//     const commentArticleEl = document.createElement("div");
+//     commentArticleEl.classList.add('comments-list');
+//     commentEntriesContainer.appendChild(commentArticleEl);
 
+//     // Icon <div class="comments-list__icon">
+//     const commentIconEl = document.createElement("div");
+//     commentIconEl.classList.add('comments-list__icon');
+//     commentArticleEl.appendChild(commentIconEl);
+
+//     // Group <div class="comments-list__group">
+//     const commentGroupEl = document.createElement("div");
+//     commentGroupEl.classList.add('comments-list__group');
+//     commentArticleEl.appendChild(commentGroupEl);
+
+//     // Group 2 <div class="comments-list__group2">
+//     const commentGroup2El = document.createElement("div");
+//     commentGroup2El.classList.add('comments-list__group2');
+//     commentGroupEl.appendChild(commentGroup2El);
+    
+//     // create name <p class="comments-list__name">Victor Pinto</p>
+//     const commentNameEl = document.createElement('p');
+//     commentNameEl.classList.add('comments-list__name');
+//     commentNameEl.innerText = commentItem.name;
+//     commentGroup2El.appendChild(commentNameEl);
+
+//     // create date <date class="comments-list__date">11/02/2023</date>
+//     const commentDateEl = document.createElement('date');
+//     commentDateEl.classList.add('comments-list__date');
+//     commentDateEl.innerText = commentItem.date;
+//     commentGroup2El.appendChild(commentDateEl);
+
+//     // create comment <p class="comments-list__comment">
+//     const commentTextEl = document.createElement('p');
+//     commentTextEl.classList.add('comments-list__comment');
+//     commentTextEl.innerText = commentItem.comment;
+//     commentGroupEl.appendChild(commentTextEl);
+
+//     console.log("commentGroupEl: ", commentGroupEl);
+//   });
+// }
+
+// // To generate dynamic date
+// function getCurrentDate() {
+//   var date = new Date();
+//   var options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+//   return date.toLocaleDateString('en-US', options);
+// }
+
+// // Form event handling callback
+// const formEl = document.querySelector('.comments-form');
+
+// formEl.addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+//   const nameVal = event.target.nameId.value;
+//   const commentVal = event.target.commentId.value;
+//   const dateVal = getCurrentDate();
+
+//   const newEntry = {
+//     name: nameVal,
+//     comment: commentVal,
+//     date: dateVal
+//   };
+
+//   console.log('newEntry: ', newEntry);
+
+//   commentEntries.push(newEntry);
+
+//   renderCommentEntries();
+
+//   // Clear all form fields from the form after submitting
+//   event.target.reset();
+
+// });
+
+// // To invoke the function to render initial comment entries on page load
+// renderCommentEntries();
+
+// // ==== Sprint 2 end ===
